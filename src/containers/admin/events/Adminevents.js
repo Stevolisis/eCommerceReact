6b6143cc9c7d15aa8b683a4204b3a0529858c69e
bob <stevolisisjoseph@gmail.com>
2022-11-03 22:37:13 +0100

    Events Reorder Done

--- src/containers/admin/events/Adminevents.js
+++ src/containers/admin/events/Adminevents.js
@@ -1,22 +1,16 @@
-import {React, useEffect, useState} from 'react';
+import {React, useEffect, useState, useRef} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { ReactSortable } from "react-sortablejs";
+import axios from 'axios';
 
 export default function Admineventcoupon(){
     const navigate=useNavigate();
-    const [state, setState] = useState([
-      { id: 1, title: "Main Banner",banner_color: "Crimson",date:"10/6/2022",products: 12, sold:3},
-      { id: 2, title: "Special Category",banner_color: "orangered",date:"10/6/2022",products: 3, sold:1},
-      { id: 3, title: "Brand Slider",banner_color: "dodgerblue",date:"10/6/2022",products: 32, sold:22},
-      { id: 4, title: "Ketchen Listen",banner_color: "brickred",date:"10/6/2022",products: 19, sold:10},
-      { id: 5, title: "I phone Ads Banner",banner_color: "violet",date:"10/6/2022",products: 55, sold:44}
-    ]);
-    // const sortedArray=Array.from(state);
-    const [sortedArray, setsortedArray] = useState(state);
+    const [order, setOrder] = useState([]);
+    const cancelalert=useRef(true)
 
 
-       const deletespec2=(()=>{
+       const deletespec2=((id)=>{
         Swal.fire({
             title: 'Are you sure?',
             text: "You won't be able to revert this!",
@@ -27,33 +21,124 @@ export default function Admineventcoupon(){
             confirmButtonText: 'Yes, delete it!'
           }).then((result) => {
             if (result.isConfirmed) {
-              Swal.fire(
-                'Deleted!',
-                'Coupon Deleted.',
-                'success'
-              )
+              let formData=new FormData();
+              formData.append('id',id)
+              axios.post('http://localhost:80/events/delete-event',formData)
+              .then(res=>{
+                let status=res.data.status;
+                let data=res.data.data;
+                  if(status==='success'){
+                    Swal.fire(
+                      'Deleted!',
+                      'Event Deleted.',
+                      'success'
+                    )
+                  }else{
+                    Swal.fire(
+                      'Error Occured',
+                      status,
+                      'warning'
+                    )
+                  }
+              }).catch(err=>{
+                Swal.fire(
+                  'Error Occured',
+                  err.message,
+                  'error'
+                )
+              })
             }
           })
        })
 
 
-       function move(array, from, to) {
-        if( to === from ) return array;
+      //  function move(array, from, to) {
+      //   if( to === from ) return array;
       
-        var target = array[from];                         
-        var increment = to < from ? -1 : 1;
+      //   var target = array[from];                         
+      //   var increment = to < from ? -1 : 1;
       
-        for(var k = from; k !== to; k += increment){
-          array[k] = array[k + increment];
+      //   for(var k = from; k !== to; k += increment){
+      //     array[k] = array[k + increment];
+      //   }
+      //   array[to] = target;
+      //   return array;
+      // }
+
+
+      const loadEvents=()=>{
+        axios.get('http://localhost:80/events/get-events')
+        .then(res=>{
+          let status=res.data.status;
+          let data=res.data.data;
+            if(status!=='success'){
+                Swal.fire(
+                    'Error Occured',
+                    status,
+                    'warning'
+                  )
+            }else{
+               data.forEach(event=>{
+                setOrder(oldOption=>[...oldOption,{id:event._id,title:event.title,type:event.type,status:event.status}])
+               })
+
+            }
+        }).catch(err=>{
+            Swal.fire(
+                'Error At Axios2!',
+                err.message,
+                'error'
+              )
+        })
+    }
+
+
+    function saveSortedArr(){
+        Swal.fire({
+        title: 'Please Confirm',
+        text: "Action will change the layout arrangment of your hompage",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, Reorder it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axios.post('http://localhost:80/events/reorder-events',{order:order})
+          .then(res=>{
+            let status=res.data.status;
+
+            if(status==='success'){
+                Swal.fire(
+                  'Reordered!',
+                  'Layout Reordered.',
+                  'success'
+                )
+              }else{
+                Swal.fire(
+                  'Error Occured',
+                  status,
+                  'warning'
+                )
+              }
+          }).catch(err=>{
+            Swal.fire(
+              'Error Occured',
+              err.message,
+              'error'
+            )
+          })
         }
-        array[to] = target;
-        return array;
-      }
+      })    }
+
 
 
       useEffect(()=>{
-        // console.log(state)
-      },[state])
+        if(cancelalert.current){
+          cancelalert.current=false;
+        loadEvents();
+        }
+      },[])
 
 
 
@@ -103,19 +188,21 @@ export default function Admineventcoupon(){
             <tr>
             <th>Arrange</th>
             <th>Title</th>
+            <th>Type</th>
             <th>Edit</th>
             <th>Delete</th>
             <th>Status</th>
             </tr>
 </tbody>
 
-          <ReactSortable tag="tbody" list={state} setList={setState} handle='.handler'>
-          {state.map((item) => (
+          <ReactSortable tag="tbody" list={order} setList={setOrder} handle='.handler'>
+          {order.map((item) => (
           <tr key={item.id} >
             <td className='handler' style={{cursor:"grab",textAlign:'center'}}><i className='fa fa-bars'/></td>
             <td>{item.title}</td>
-            <td><Link to='/admin/editcoupon/wdhfvj44'><i className='fa fa-edit'/></Link></td>
-            <td><button onClick={()=>deletespec2()}>Delete</button></td>
+            <td>{item.type}</td>
+            <td><Link to={`/admin/editevent/${item.id}`}><i className='fa fa-edit'/></Link></td>
+            <td><button onClick={()=>deletespec2(item.id)}>Delete</button></td>
             <td>{item.status}</td>
           </tr>
           ))}
@@ -128,7 +215,7 @@ export default function Admineventcoupon(){
             </div>
         </div>
         <div className='adminmorebtn'>
-            <button>See More</button>
+            <button onClick={saveSortedArr}>Save</button>
         </div>
         </div>
 
