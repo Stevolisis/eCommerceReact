bc2072a98ed658f15a50b7a47ae1281d86d08d15
bob <stevolisisjoseph@gmail.com>
2022-11-07 19:37:14 +0100

    axios Config baseUrl changed to live api

--- src/Utils/axiosConfig.js
+++ src/Utils/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL:"http://localhost:80",
+    baseURL:"https://ecommerce-api-ivory.vercel.app",
+    // baseURL:"http://localhost:80",
 });
 
 export default api;
