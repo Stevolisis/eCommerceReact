60f22c7ba8d3a508e5c7dabaa1d440aa05d120f5
Steven Joseph <stevolisisjoseph@gmail.com>
2023-02-15 18:16:31 +0100

    product page build4

--- src/containers/main/Product.js
+++ src/containers/main/Product.js
@@ -78,6 +78,7 @@ export default function Product(){
             setDatas({
                 name:'Related Products',
                 product_component:{
+                    banner_color:'#5972b9',
                     products:relProducts
                 }
 
