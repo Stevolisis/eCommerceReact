d4f4a20ea3ca48522cef78a7ee2a8b72bdf7a771
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-08 13:06:34 +0100

    usersList

--- src/components/listings/UsersList.js
+++ src/components/listings/UsersList.js
@@ -8,21 +8,27 @@ export default function UsersList({deleteuser}){
 
     const userLists=users.map((user,i)=>{
         let {_id,first_name,last_name,email,phone_number,orders,wishlist,createdAt,verified,status}=user;
-        return(           
-            <tr key={i}>
-            <td>{first_name+' '+last_name}</td>
-            <td>{email}</td>
-            <td>{phone_number}</td>
-            <td>{orders.length===0 ? 0 : orders}</td>
-            <td>{wishlist.length===0 ? 0 : wishlist}</td>
-            <td>{verified===true ? 'true' : 'false'}</td>
-            <td>{createdAt.split('T')[0]}</td>
-            <td><Link to={`/admin/edituser/${_id}`}><i className='fa fa-edit'/></Link></td>
-            <td><button onClick={()=>deleteuser(_id)}>Delete</button></td>
-            <td>{status}</td>
-            </tr>
+        return(   <>HI</>        
+            // <tr key={i}>
+            // <td>{first_name+' '+last_name}</td>
+            // <td>{email}</td>
+            // <td>{phone_number}</td>
+            // <td>{orders.length===0 ? 0 : orders}</td>
+            // <td>{wishlist.length===0 ? 0 : wishlist}</td>
+            // <td>{verified===true ? 'true' : 'false'}</td>
+            // <td>{createdAt.split('T')[0]}</td>
+            // <td><Link to={`/admin/edituser/${_id}`}><i className='fa fa-edit'/></Link></td>
+            // <td><button onClick={()=>deleteuser(_id)}>Delete</button></td>
+            // <td>{status}</td>
+            // </tr>
         )
-     })
+     });
+
+
+
+
+
+     
     return(
         <>
             <tbody>
