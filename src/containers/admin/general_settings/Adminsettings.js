2cbcd7440a2936f3bc5e35b87e51b22de179327d
Steven Joseph <stevolisisjoseph@gmail.com>
2023-05-11 23:31:33 +0100

    Navigating buttons: index, payment_gateway and support

--- src/containers/admin/general_settings/Adminsettings.js
+++ src/containers/admin/general_settings/Adminsettings.js
@@ -1,11 +1,12 @@
 import {React} from 'react';
 import PaymentGateway from '../../../components/general_settings/payment_method';
 import Support from '../../../components/general_settings/support';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useMemo } from 'react';
 
 export default function Adminpayment(){
     const { section }=useParams();
+    const navigate=useNavigate();
 
     useMemo(()=>{
 
@@ -16,6 +17,11 @@ export default function Adminpayment(){
         <div className='admindashcon'>
             <div className='userorderheading'>
                 <p>Payment Methods</p>
+            </div>
+            <div>
+                <button onClick={()=>navigate('/admin/general_settings/index')}>General Settings</button>
+                <button onClick={()=>navigate('/admin/general_settings/payment_gateway')}>Payment Gateway</button>
+                <button onClick={()=>navigate('/admin/general_settings/support')}>Support</button>
             </div>
 
             {
