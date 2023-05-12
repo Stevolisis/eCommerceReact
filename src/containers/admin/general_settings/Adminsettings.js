9098631686fff61e7379b32e38123fcbd658789c
Steven Joseph <stevolisisjoseph@gmail.com>
2023-05-12 12:59:35 +0100

    Switch navigation done

--- src/containers/admin/general_settings/Adminsettings.js
+++ src/containers/admin/general_settings/Adminsettings.js
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import {React, useMemo, useState} from 'react';
 import PaymentGateway from '../../../components/general_settings/payment_method';
 import Support from '../../../components/general_settings/support';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -12,7 +12,13 @@ export default function Adminpayment(){
         navigate(`/admin/general_settings/${route}`); 
         setActive(id);
     }
-    console.log(active)
+    
+    
+    useMemo(()=>{
+        if(section==='index') setActive(1)
+        if(section==='payment_gateway') setActive(2)
+        if(section==='support') setActive(3)
+    },[section])
 
     return(
         <>
@@ -22,12 +28,14 @@ export default function Adminpayment(){
             </div>
             <div className='settingsNavigateCon'>
                 <button className='settingsNavigateBtn' 
-                style={{borderBottom:`${active===1 && '3px solid #758db9'}`,background:`${active===1 && '#faf9f9'}`}} 
+                    style={{borderBottom:`${active===1 ? '3px solid #758db9' : 'none'}`,background:`${active===1 ? 'white' : '#faf9f9'}`}} 
                 onClick={()=>SwitchScreen(1,'index')}>
                 General Settings</button>
-                <button className='settingsNavigateBtn' style={{borderBottom:`${active===2 && '3px solid #758db9'}`,background:`${active===2 && '#faf9f9'}`}} 
+                <button className='settingsNavigateBtn' 
+                    style={{borderBottom:`${active===2 ? '3px solid #758db9' : 'none'}`,background:`${active===2 ? 'white' : '#faf9f9'}`}} 
                 onClick={()=>SwitchScreen(2,'payment_gateway')}>Payment Gateway</button>
-                <button className='settingsNavigateBtn' sstyle={{borderBottom:`${active===3 && '3px solid #758db9'}`,background:`${active===3 && '#faf9f9'}`}}
+                <button className='settingsNavigateBtn' 
+                    style={{borderBottom:`${active===3 ? '3px solid #758db9' : 'none'}`,background:`${active===3 ? 'white' : '#faf9f9'}`}}
                 onClick={()=>SwitchScreen(3,'support')}>Support</button>
             </div>
 
